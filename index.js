caeddf10cbbf622b70e062bac0a4fba4943250f2
Stephen Bayne <sephizor@sephizor.com>
2021-03-01 17:21:45 +0000

    Add eslint and fix errors

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
-import { Builder, By, until } from 'selenium-webdriver';
+import {Builder, By, until} from 'selenium-webdriver';
 import nodeNotifier from 'node-notifier';
-import { Options } from 'selenium-webdriver/chrome.js';
+import {Options} from 'selenium-webdriver/chrome.js';
 import winston from 'winston';
 import open from 'open';
 import fs from 'fs';
 import path from 'path';
 
-const settings = JSON.parse(fs.readFileSync('settings.json'));
+(async function findRtx() {
+  const settings = JSON.parse(fs.readFileSync('settings.json'));
 
-if(fs.existsSync('settings.local.json')) {
+  if (fs.existsSync('settings.local.json')) {
     Object.assign(settings, JSON.parse(fs.readFileSync('settings.local.json')));
-}
+  }
 
-const logger = winston.createLogger({
+  const logger = winston.createLogger({
     transports: [
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.timestamp(),
-                winston.format.align(),
-                winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
-            )
-        })
-    ]
-});
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.timestamp(),
+          winston.format.align(),
+          winston.format.printf((info) => `${info.timestamp} [${info.level}]: ${info.message}`),
+        ),
+      }),
+    ],
+  });
 
-if(settings.login && (settings.amazonPassword === '' || settings.amazonUsername === '')) {
+  if (settings.login && (settings.amazonPassword === '' || settings.amazonUsername === '')) {
     logger.error('Login requested but username or password are empty');
     process.exit(-1);
-}
-if(settings.autobuy && (!settings.login || settings.amazonUsername === '' || settings.amazonPassword === '')) {
+  }
+  if (settings.autobuy && (!settings.login || settings.amazonUsername === '' || settings.amazonPassword === '')) {
     logger.error('Autobuy requires login to be set as well as your Amazon username and password');
     process.exit(-1);
-}
+  }
 
-let bought = 0;
+  let bought = 0;
 
-const chromeOptions = new Options();
-chromeOptions.headless();
-chromeOptions.windowSize({ width: 1920, height: 1080 });
-chromeOptions.addArguments([
+  const chromeOptions = new Options();
+  chromeOptions.headless();
+  chromeOptions.windowSize({width: 1920, height: 1080});
+  chromeOptions.addArguments([
     '--disable-gpu',
     '--disable-logging',
     '--log-level=3',
     '--disable-crash-reporter',
-    '--disable-in-process-stack-traces'
-]);
+    '--disable-in-process-stack-traces',
+  ]);
 
-const driver = await new Builder()
+  const driver = await new Builder()
     .forBrowser('chrome')
     .setChromeOptions(chromeOptions)
     .build();
 
-async function login() {
+  /**
+   * @description Logs into your Amazon account
+   */
+  async function login() {
     await driver.get('https://amazon.co.uk/');
     logger.info('Logging in');
     await driver.findElement(By.id('nav-link-accountList-nav-line-1')).click();
@@ -63,91 +67,112 @@ async function login() {
     await driver.findElement(By.id('ap_password')).sendKeys(settings.amazonPassword);
     await driver.findElement(By.id('signInSubmit')).click();
     logger.info('Completed login');
-}
+  }
 
-async function checkForButton() {
+  /**
+   * @description Checks for the existence of the "Buy Now" button on an Amazon product page
+   */
+  async function checkForButton() {
     const elements = await driver.findElements(By.id('buy-now-button'));
     if (elements.length > 0) {
-        return true;
+      return true;
     }
     return false;
-}
+  }
 
-async function getPrice() {
+  /**
+   * @description Retrieves the price of the product from the right-hand product information panel
+   * @return {number} The price of the card
+   */
+  async function getPrice() {
     const element = await driver.findElement(By.id('price_inside_buybox'));
-    if(await element.isDisplayed()) {
-        return parseFloat((await element.getText()).replace('£', ''));
+    if (await element.isDisplayed()) {
+      return parseFloat((await element.getText()).replace('£', ''));
     }
     return -1;
-}
+  }
 
-async function buyCard() {
-    if(bought < settings.autobuyLimit) {
-        await driver.findElement(By.id('buy-now-button')).click();
-        await driver.findElement(By.id('turbo-checkout-pyo-button')).click();
-        bought++;
+  /**
+   * @description Clicks the "Buy Now" button with the default shipping address and billing details set up
+   * On your Amazon account
+   */
+  async function buyCard() {
+    if (bought < settings.autobuyLimit) {
+      await driver.findElement(By.id('buy-now-button')).click();
+      await driver.findElement(By.id('turbo-checkout-pyo-button')).click();
+      bought++;
+      await driver.sleep(5000);
     }
-}
+  }
 
-async function checkCard(cardName, url, maxPrice) {
+  /**
+   * @description Main function for checking the existence of a product
+   * @param {string} cardName
+   * @param {string} url
+   * @param {number} maxPrice
+   */
+  async function checkCard(cardName, url, maxPrice) {
     logger.info(`Checking ${cardName}`);
     await driver.get(url);
     const inStock = await checkForButton();
-    if(inStock) {
-        const price = await getPrice();
-        if(price === -1) {
-            logger.warn(`Found card ${cardName} but could not parse price!`);
-            nodeNotifier.notify({
-                title: 'RTX Finder',
-                message: `Found card ${cardName} but could not parse price!`
-            });
+    if (inStock) {
+      const price = await getPrice();
+      if (price === -1) {
+        logger.warn(`Found card ${cardName} but could not parse price!`);
+        nodeNotifier.notify({
+          title: 'RTX Finder',
+          message: `Found card ${cardName} but could not parse price!`,
+        });
+      } else if (maxPrice === 0 || price <= maxPrice) {
+        logger.warn(`Found card ${cardName} for £${price.toFixed(2)}`);
+        nodeNotifier.notify({
+          title: 'RTX Finder',
+          message: `Found card ${cardName} for £${price.toFixed(2)}`,
+        }, () => {
+          open(url);
+        });
+        if (settings.autobuy) {
+          await buyCard();
         }
-        else if(maxPrice === 0 || price <= maxPrice) {
-            logger.warn(`Found card ${cardName} for ${price.toFixed(2)}`);
-            nodeNotifier.notify({
-                title: 'RTX Finder',
-                message: `Found card ${cardName} for £${price.toFixed(2)}`,
-            }, () => {
-                open(url);
-            });
-            if(settings.autobuy) {
-                await buyCard();
-            }
-            await takeScreenshot();
-        }
+        await takeScreenshot();
+      } else {
+        logger.error(`Found card ${cardName} for £${price.toFixed(2)} but this was above your specified price! :<`);
+      }
     }
     logger.info(`Finished checking ${cardName}`);
-}
+  }
 
-async function takeScreenshot() {
-    if(settings.takeScreenshots) {
-        const png = await driver.takeScreenshot();
-        if(!fs.existsSync('screenshots')) {
-            fs.mkdirSync('screenshots');
-        }
-        const date = Intl.DateTimeFormat('en-GB', {
-            day: 'numeric', month: 'numeric', year: 'numeric',
-            hour: 'numeric', minute: 'numeric', second: 'numeric'
-        }).formatToParts(new Date());
-        fs.writeFileSync(`screenshots${path.sep}${date[0].value}-${date[2].value}-${date[4].value}_${date[6].value}-${date[8].value}-${date[10].value}.png`, png, 'base64');
+  /**
+   * @description Takes a screenshot of the current page
+   */
+  async function takeScreenshot() {
+    if (settings.takeScreenshots) {
+      const png = await driver.takeScreenshot();
+      if (!fs.existsSync('screenshots')) {
+        fs.mkdirSync('screenshots');
+      }
+      const date = new Intl.DateTimeFormat('en-GB', {
+        day: 'numeric', month: 'numeric', year: 'numeric',
+        hour: 'numeric', minute: 'numeric', second: 'numeric',
+      }).formatToParts(new Date());
+      fs.writeFileSync(`screenshots${path.sep}${date[0].value}-${date[2].value}-${date[4].value}_${date[6].value}-` +
+        `${date[8].value}-${date[10].value}.png`, png, 'base64');
     }
-}
+  }
 
-(async function findRtx() {
-    try {
-        if(settings.login) {
-            await login();
-        }
-        while (true) {
-            for(const card of settings.cards) {
-                const maxPrice = card.maxPrice !== undefined ? card.maxPrice : settings.maxPrice;
-                await checkCard(card.name, card.url, maxPrice);
-            }
-            logger.info(`Finished checking all cards; sleeping for ${settings.sleepTime} seconds`);
-            await driver.sleep(1000 * settings.sleepTime);
-        }
+  try {
+    if (settings.login) {
+      await login();
     }
-    finally {
-        await driver.quit();
+    while (true) {
+      for (const card of settings.cards) {
+        const maxPrice = card.maxPrice !== undefined ? card.maxPrice : settings.maxPrice;
+        await checkCard(card.name, card.url, maxPrice);
+      }
+      logger.info(`Finished checking all cards; sleeping for ${settings.sleepTime} seconds`);
+      await driver.sleep(1000 * settings.sleepTime);
     }
+  } finally {
+    await driver.quit();
+  }
 })();
